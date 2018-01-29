309753f4286d3367302eeea2f0173b4730063728
conamatic <35837333+conamatic@users.noreply.github.com>
2018-01-29 14:41:30 +0000

    Add project files.

---
+++ Dispatcher Tool/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System.Drawing;
+
+namespace TestTool
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addMultipleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Export_Config_Dialog = new System.Windows.Forms.SaveFileDialog();
+            this.Import_File_Dialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.runToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.addMultipleToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.importToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.clearToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // addMultipleToolStripMenuItem
+            // 
+            this.addMultipleToolStripMenuItem.Name = "addMultipleToolStripMenuItem";
+            this.addMultipleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addMultipleToolStripMenuItem.Text = "Add multiple...";
+            this.addMultipleToolStripMenuItem.Click += new System.EventHandler(this.addMultipleToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.runToolStripMenuItem.Text = "Run";
+            // 
+            // Export_Config_Dialog
+            // 
+            this.Export_Config_Dialog.DefaultExt = "cfg";
+            this.Export_Config_Dialog.FileName = "config";
+            this.Export_Config_Dialog.InitialDirectory = "C:\\WW4";
+            this.Export_Config_Dialog.Title = "Export...";
+            // 
+            // Import_File_Dialog
+            // 
+            this.Import_File_Dialog.DefaultExt = "cfg";
+            this.Import_File_Dialog.FileName = "config";
+            this.Import_File_Dialog.Filter = "|*cfg";
+            this.Import_File_Dialog.InitialDirectory = "C:\\WW4";
+            this.Import_File_Dialog.Title = "Import...";
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(462, 496);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = new Icon("DispatchViewer.ico");
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main_Form";
+            this.Text = "Dispatcher Tool";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addMultipleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog Export_Config_Dialog;
+        private System.Windows.Forms.OpenFileDialog Import_File_Dialog;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Dispatcher Tool/Form2.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TestTool
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Quantity_Input = new System.Windows.Forms.TextBox();
+            this.Quantity_Label = new System.Windows.Forms.Label();
+            this.Template_Label = new System.Windows.Forms.Label();
+            this.Template_Input = new System.Windows.Forms.TextBox();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.Cancel_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Quantity_Input
+            // 
+            this.Quantity_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Quantity_Input.Location = new System.Drawing.Point(125, 41);
+            this.Quantity_Input.Name = "Quantity_Input";
+            this.Quantity_Input.Size = new System.Drawing.Size(73, 23);
+            this.Quantity_Input.TabIndex = 0;
+            // 
+            // Quantity_Label
+            // 
+            this.Quantity_Label.AutoSize = true;
+            this.Quantity_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Quantity_Label.Location = new System.Drawing.Point(26, 44);
+            this.Quantity_Label.Name = "Quantity_Label";
+            this.Quantity_Label.Size = new System.Drawing.Size(61, 17);
+            this.Quantity_Label.TabIndex = 1;
+            this.Quantity_Label.Text = "Quantity";
+            // 
+            // Template_Label
+            // 
+            this.Template_Label.AutoSize = true;
+            this.Template_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Template_Label.Location = new System.Drawing.Point(26, 76);
+            this.Template_Label.Name = "Template_Label";
+            this.Template_Label.Size = new System.Drawing.Size(67, 17);
+            this.Template_Label.TabIndex = 2;
+            this.Template_Label.Text = "Template";
+            // 
+            // Template_Input
+            // 
+            this.Template_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Template_Input.Location = new System.Drawing.Point(125, 73);
+            this.Template_Input.Name = "Template_Input";
+            this.Template_Input.Size = new System.Drawing.Size(238, 23);
+            this.Template_Input.TabIndex = 3;
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Location = new System.Drawing.Point(288, 134);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(75, 23);
+            this.Save_Button.TabIndex = 4;
+            this.Save_Button.Text = "OK";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // Cancel_Button
+            // 
+            this.Cancel_Button.Location = new System.Drawing.Point(207, 134);
+            this.Cancel_Button.Name = "Cancel_Button";
+            this.Cancel_Button.Size = new System.Drawing.Size(75, 23);
+            this.Cancel_Button.TabIndex = 5;
+            this.Cancel_Button.Text = "Cancel";
+            this.Cancel_Button.UseVisualStyleBackColor = true;
+            this.Cancel_Button.Click += new System.EventHandler(this.Cancel_Button_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 169);
+            this.Controls.Add(this.Cancel_Button);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.Template_Input);
+            this.Controls.Add(this.Template_Label);
+            this.Controls.Add(this.Quantity_Label);
+            this.Controls.Add(this.Quantity_Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form2";
+            this.Text = "Add Multiple";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Quantity_Input;
+        private System.Windows.Forms.Label Quantity_Label;
+        private System.Windows.Forms.Label Template_Label;
+        private System.Windows.Forms.TextBox Template_Input;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button Cancel_Button;
+    }
+}
